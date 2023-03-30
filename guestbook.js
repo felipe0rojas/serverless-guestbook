98c068a0cc9fa1a2330f7172fce82dfab3f269ba
felipe0rojas <frbandera@gmail.com>
2023-03-29 22:09:53 -0300

    Fixed typo

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/edc6af48-6da1-4216-aca4-05fbd413161f/guestbook/';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/edc6af48-6da1-4216-aca4-05fbd413161f/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
